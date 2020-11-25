b9812d5f85d535f21f5e8811cf9631a483fe674a
Thomas Wirth <thomas-w-@web.de>
2020-11-25 01:49:22 +0100

    Set up publishing

--- quickie/build.gradle.kts
+++ quickie/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   id(Plugins.Android.library)
   kotlin(Plugins.Kotlin.android)
   kotlin(Plugins.Kotlin.parcelize)
+  `maven-publish`
 }
 
 android {
@@ -61,4 +62,59 @@ dependencies {
 
   bundledImplementation(Deps.MLKit.barcodeScanning)
   unbundledImplementation(Deps.MLKit.barcodeScanningGms)
+}
+
+group = "com.github.g00fy2" // TODO sync final groupId and package names
+version = "0.1.0"
+
+// TODO add Dokka and javadocJar task
+
+tasks.register<Jar>("androidSourcesJar") {
+  archiveClassifier.set("sources")
+  from(android.sourceSets.getByName("main").java.srcDirs)
+}
+
+afterEvaluate {
+  publishing {
+    publications {
+      create<MavenPublication>("bundledRelease") {
+        from(components["bundledRelease"])
+        artifactId = "quickie-bundled"
+        artifact(tasks.named("androidSourcesJar"))
+        configurePom()
+      }
+      create<MavenPublication>("unbundledRelease") {
+        from(components["unbundledRelease"])
+        artifactId = "quickie-unbundled"
+        artifact(tasks.named("androidSourcesJar"))
+        configurePom()
+      }
+    }
+  }
+}
+
+fun MavenPublication.configurePom() {
+  pom {
+    name.set(project.name)
+    description.set("Android QR code scanner library")
+    url.set("https://github.com/G00fY2/Quickie")
+    licenses {
+      license {
+        name.set("The MIT License")
+        url.set("https://opensource.org/licenses/MIT")
+      }
+    }
+    developers {
+      developer {
+        id.set("g00fy2")
+        name.set("Thomas Wirth")
+        email.set("twirth.development@gmail.com")
+      }
+    }
+    scm {
+      connection.set("https://github.com/G00fY2/Quickie.git")
+      developerConnection.set("https://github.com/G00fY2/Quickie.git")
+      url.set("https://github.com/G00fY2/Quickie")
+    }
+  }
 }
