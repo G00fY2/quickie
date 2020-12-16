efa0a484971206fa213280890a7d588ce3f1856c
Thomas Wirth <thomas-w-@web.de>
2020-12-16 23:27:08 +0100

    Update deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,16 +8,16 @@ object Versions {
   const val gradleVersions = "0.36.0"
   const val dokka = "1.4.20"
 
-  const val activity = "1.2.0-beta02"
-  const val fragment = "1.3.0-beta02"
+  const val activity = "1.2.0-rc01"
+  const val fragment = "1.3.0-rc01"
   const val appcompat = "1.2.0"
   const val core = "1.3.2"
 
   const val navigation = "2.3.2"
 
-  const val camera = "1.0.0-beta12"
-  const val cameraLifecycle = "1.0.0-beta12"
-  const val cameraPreview = "1.0.0-alpha19"
+  const val camera = "1.0.0-rc01"
+  const val cameraLifecycle = "1.0.0-rc01"
+  const val cameraPreview = "1.0.0-alpha20"
 
   const val materialDesign = "1.2.1"
 
