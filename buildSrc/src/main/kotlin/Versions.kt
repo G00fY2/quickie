4c9740c446921c602471c0d3eacd6cf3e934b7f2
Thomas Wirth <thomas-w-@web.de>
2021-05-02 12:04:48 +0200

    Update detekt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -21,7 +21,7 @@ object Versions {
   const val barcodeScanning = "16.1.1"
   const val barcodeScanningGms = "16.1.4"
 
-  const val detekt = "1.16.0"
+  const val detekt = "1.17.0-RC1"
   const val gradleVersions = "0.38.0"
   const val dokka = "1.4.32"
 
