88a66a59150dcbdd8dab9a7915c6533f455ffb72
Thomas Wirth <thomas-w-@web.de>
2021-05-14 01:20:43 +0200

    Update AGP and detekt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
   const val androidTargetSdk = 30
   const val androidBuildTools = "30.0.3"
 
-  const val androidGradle = "4.2.0"
+  const val androidGradle = "4.2.1"
   const val kotlin = "1.5.0"
 
   const val activity = "1.2.3"
@@ -21,7 +21,7 @@ object Versions {
   const val barcodeScanning = "16.1.1"
   const val barcodeScanningGms = "16.1.4"
 
-  const val detekt = "1.17.0-RC2"
+  const val detekt = "1.17.0-RC3"
   const val gradleVersions = "0.38.0"
   const val dokka = "1.4.32"
 
