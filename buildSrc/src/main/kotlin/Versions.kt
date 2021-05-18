4d36d1763a2c582eb987e01dbb3a0ef1675ceb6a
Thomas Wirth <thomas-w-@web.de>
2021-05-18 22:13:59 +0200

    Update to appcompat 1.3.0 stable

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
   const val androidGradle = "4.2.1"
   const val kotlin = "1.5.0"
 
-  const val appcompat = "1.3.0-rc01"
+  const val appcompat = "1.3.0"
 
   const val cameraX = "1.0.0"
   const val cameraView = "1.0.0-alpha24"
