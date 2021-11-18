582874413e67393973398b06a2e2972b4a8e680c
Thomas Wirth <thomas-w-@web.de>
2021-11-18 10:35:22 +0100

    Set buildSrc jvm target to 1.8

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,18 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
   `kotlin-dsl`
 }
 
 repositories {
   mavenCentral()
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+  kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<JavaCompile>().configureEach {
+  sourceCompatibility = "1.8"
+  targetCompatibility = "1.8"
 }
