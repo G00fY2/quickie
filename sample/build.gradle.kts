d0198cd41bf47a8b375d887a42e38b52f730bb14
Thomas Wirth <thomas-w-@web.de>
2022-10-01 13:58:54 +0200

    Use alias to apply plugins in sample app

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,7 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-  id(libs.plugins.android.application.get().pluginId)
-  id(libs.plugins.kotlin.android.get().pluginId)
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
 }
 
 android {
