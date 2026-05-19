f103a1a10d9f3b7bfae6a8b39f5b899fbba81607
Thomas Wirth <thomas-w-@web.de>
2026-05-19 23:32:29 +0200

    Switch to liberica and defined ubuntu runner version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.parcelize) apply false
-  alias(libs.plugins.kotlin.dokka) apply false
+  alias(libs.plugins.kotlin.dokka.javadoc) apply false
   alias(libs.plugins.detekt) apply false
 }
 
